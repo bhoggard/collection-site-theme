e61f0890725905db1e4fd44f92a4ce3fabebe18e
Barry Hoggard <barry@tristanmedia.com>
2017-09-07 01:13:52 +0000

    Add favicon

--- views/pageFormat/pageHeader.php
+++ views/pageFormat/pageHeader.php
@@ -54,6 +54,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+	<link rel="shortcut icon" type="image/x-icon" href="/themes/hoggardwagner/assets/pawtucket/graphics/favicon.png" />
 	<?php print MetaTagManager::getHTML(); ?>
 	<?php print AssetLoadManager::getLoadHTML($this->request); ?>
 
